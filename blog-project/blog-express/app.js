d51ef315c1885488ecce8f01efe6ba831eba42a7
YoNgWeI <30373219+YoNgWeI1992@users.noreply.github.com>
2021-10-16 21:23:38 +0800

    express 实现登录,controller

--- blog-project/blog-express/app.js
+++ blog-project/blog-express/app.js
@@ -1,30 +1,66 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const session = require('express-session')
+const RedisStore = require('connect-redis')(session)
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// const indexRouter = require('./routes/index');
+// const usersRouter = require('./routes/users');
 const blogRouter = require('./routes/blog')
 const userRouter = require('./routes/user')
 
-var app = express();
+const app = express();
 
-// view engine setup
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'jade');
+// 写入access日志
+const ENV = process.env.NODE_ENV
+if(ENV !== 'production') {
+  // 开发/测试 环境
+  app.use(logger('dev', {
+    stream: process.stdout // 默认配置
+  }));
+}else {
+  // 线上环境
+  const logFileName = path.join(__dirname, 'logs', 'access.log')
+  const writeStream = fs.createWriteStream(logFileName,{
+    flags: 'a'
+  })
+  app.use(logger('combined', {
+    stream: writeStream
+  }));
+}
 
-app.use(logger('dev'));
+
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 // app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+const redisClient = require('./db/redis')
+const sessionStore = new RedisStore({
+  client: redisClient
+})
+
+// 解析 session
+app.use(session({
+  // resave: false,
+  // saveUninitialized: true,
+  secret: 'WJiol_#9089_',
+  cookie: {
+    // path: '/',      // 默认配置
+    // httpOnly: true, // 默认配置
+    maxAge: 24 * 60 * 60 * 1000
+  },
+  store: sessionStore
+}))
+
+// app.use('/', indexRouter); 
+// app.use('/users', usersRouter);
 app.use('/api/blog/', blogRouter)
-app.use('/user', userRouter)
+app.use('/api/user/', userRouter)
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
@@ -33,6 +69,9 @@ app.use(function(req, res, next) {
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
+
+  console.log('err: ', err);
+
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
--- blog-project/blog-express/routes/blog.js
+++ blog-project/blog-express/routes/blog.js
@@ -1,18 +1,95 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const { 
+	getList, 
+	getDetail,
+	newBlog,
+	updateBlog,
+	delBlog 
+} = require('../controller/blog')
+const { SuccessModel, ErrorModel } = require('../model/resModel')
+const loginCheck = require('../middleware/loginCheck')  
 
 router.get('/list', function(req, res, next) {
-  res.json({
-		errno:0,
-		data: [1,2,3]
-	});
+  let author = req.query.author || ''
+	const keyword = req.query.keyword || ''
+
+	if(req.query.isadmin) {
+		console.log('is admin')
+		if(req.session.username === null) {
+			console.error('is admin, but no login')
+			// 未登录
+			res.json(
+				new ErrorModel('未登录')
+			)
+			return
+		}
+		author = req.session.username 
+	}
+
+	const result = getList(author, keyword)
+	return result.then(listData => {
+		res.json(
+			new SuccessModel(listData)
+		)
+	})
 });
 
 router.get('/detail', (req, res, next) => {
-	res.json({
-		errno:0,
-		data: 'OK'
-	});
+	const result = getDetail(req.query.id)
+	return result.then(data => {
+		res.json(
+			new SuccessModel(data[0])
+		)
+	})
+})
+
+router.post('/new', loginCheck, (req, res, next) => {
+	const author = req.session.username
+	req.body.author = author
+
+	return newBlog(req.body).then(insertData => {
+		
+		res.json(
+			new SuccessModel({
+				id: insertData.insertId
+			})
+		)
+	}).catch(err => {
+		console.log(err);
+	})
+})
+
+router.post('/update', loginCheck, (req, res, next) => {
+	const result = updateBlog(req.query.id, req.body)
+	return result.then(updateData => {
+		if(updateData.affectedRows > 0)	{
+			res.json(
+				new SuccessModel('')
+			)
+		}
+		else {
+			res.json(
+				new ErrorModel('更新博客失败')
+			)
+		}
+	})
+})
+
+router.post('/del', (req, res, next) => {
+	const author = req.session.username
+	const result = delBlog(req.query.id, author)
+		return result.then(delData => {
+			if(delData.affectedRows > 0) {
+				res.json(
+					new SuccessModel()
+				)
+				return
+			}
+			res.json(
+				new ErrorModel('删除博客失败!')
+			)
+		})
 })
 
 module.exports = router;
